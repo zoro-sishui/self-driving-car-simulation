1b18fc8e491eb87db6c31476f471b79acc2c367c
MohammedSShaikh <msshaikh10@myseneca.ca>
2026-04-12 07:33:31 -0400

    fix: variable naming fixed

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -22,7 +22,7 @@ def load_data():
         image_paths, steerings, test_size=0.2, random_state=42
     )
 
-    print(f'Training samples:   {len(X_train)}')
+    print(f'Training samples: {len(X_train)}')
     print(f'Test samples: {len(X_test)}')
 
     return X_train, X_test, y_train, y_test
